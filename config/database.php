b0ac0d9eeb35280c7a23d71e70ee054e9233da06
nmfzone <nabilftd@gmail.com>
2016-04-06 00:44:15 +0700

    Set PHP error reporting to 0

--- config/database.php
+++ config/database.php
@@ -5,5 +5,6 @@ $username = "root";
 $password = "";
 $hostname = "localhost"; 
 
+error_reporting(0);
 $connection = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
 $db = mysql_select_db($database, $connection) or die("Could not select database");
